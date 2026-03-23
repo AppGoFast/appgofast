ff084fc0496fb7511daf52c3c60d5c50346d3301
Simas <simonas.tarasauskas@gmail.com>
2026-03-23 10:42:04 +0100

    api key option in settings

--- pages/settings.py
+++ pages/settings.py
@@ -8,15 +8,19 @@ class SettingsPage(ctk.CTkFrame):
         super().__init__(master)
         self.app_config = self.master.get_config()
         self.reporter_path_var = StringVar(self, value=self.app_config["reporter_path"])
+        self.api_key_var = StringVar(self, value=self.app_config["api_key"])
 
         self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure((1, 2), weight=1)
 
         self.title = ctk.CTkLabel(self, text="Settings:", font=("TkTextFont",18))
         self.title.grid(row=0, column=0, padx=20, pady=10, sticky="n")
 
         self.reporter_path = PathSelectionFrame(self, name="Reporter path:", path=self.reporter_path_var)
-        self.reporter_path.grid(row=1, column=0, sticky="we", padx=10)
+        self.reporter_path.grid(row=1, column=0, sticky="esw", padx=10, pady=5)
+
+        self.api_key = TextVarFrame(self, name="API key:", text_var=self.api_key_var)
+        self.api_key.grid(row=2, column=0, sticky="wne", padx=10, pady=5)
 
         self.bottom_buttons = BottomButtonsFrame(self, name1="Discard & Return", command1=self.on_cancel_button, name2="Save & Return", command2=self.on_save_button)
         self.bottom_buttons.grid(row=99, column=0, pady=10, padx=10, sticky="s")
@@ -24,11 +28,13 @@ class SettingsPage(ctk.CTkFrame):
 
     def on_save_button(self):
         self.app_config["reporter_path"] = self.reporter_path_var.get()
+        self.app_config["api_key"] = self.api_key_var.get()
         self.master.write_config(self.app_config)
         self.master.set_page("HomePage")
 
     def on_cancel_button(self):
         self.reporter_path_var.set(self.app_config["reporter_path"])
+        self.api_key_var.set(self.app_config["api_key"])
         self.master.set_page("HomePage")
 
 
@@ -52,6 +58,20 @@ class PathSelectionFrame(ctk.CTkFrame):
         path = select_file_dialog()
         if path:
             self.path_var.set(path)
+
+
+class TextVarFrame(ctk.CTkFrame):
+    def __init__(self, master, name, text_var):
+        super().__init__(master)
+        self.text_var = text_var
+
+        self.grid_columnconfigure(1, weight=1)
+
+        self.label = ctk.CTkLabel(self, text=name)
+        self.label.grid(row=0, column=0, sticky="w", padx=(10, 5))
+
+        self.var_entry = ctk.CTkEntry(self, textvariable=self.text_var)
+        self.var_entry.grid(row=0, column=1, sticky="we")
 
 
 class BottomButtonsFrame(ctk.CTkFrame):
