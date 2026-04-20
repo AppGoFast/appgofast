32e9a53b05e6410b2751a4ae60cf52352c0762e1
Simas <simonas.tarasauskas@gmail.com>
2026-04-20 12:44:53 +0200

    better settings structure, scroll

--- pages/settings.py
+++ pages/settings.py
@@ -12,55 +12,35 @@ class SettingsPage(ctk.CTkFrame):
         self.reporter_path_var = StringVar(self, value=self.app_config["reporter_path"])
         self.api_key_var = StringVar(self, value=self.app_config["api_key"])
         self.ai_models = self.app_config["ai_models"]
-        self.selected_ai_model = StringVar(self, value=self.app_config["selected_ai_model"])
+        self.selected_ai_model_var = StringVar(self, value=self.app_config["selected_ai_model"])
         self.ai_prompt_var = StringVar(self, value=self.app_config["ai_prompt"])
 
-        self.is_dual_model = StringVar(self, value=self.app_config["dual_ai_model"])
-        self.ai2_include_profiler_output = StringVar(self, value=self.app_config["ai2_include_profiler_output"])
-        self.selected_ai_model2 = StringVar(self, value=self.app_config["selected_ai_model2"])
-        self.ai_prompt_var2 = StringVar(self, value=self.app_config["ai_prompt2"])
+        self.is_dual_model_var = StringVar(self, value=self.app_config["dual_ai_model"])
+        self.ai2_include_profiler_output_var = StringVar(self, value=self.app_config["ai2_include_profiler_output"])
+        self.selected_ai_model2_var = StringVar(self, value=self.app_config["selected_ai_model2"])
+        self.ai_prompt2_var = StringVar(self, value=self.app_config["ai_prompt2"])
 
         self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure((1, 98), weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
-
-        self.reporter_path = PathSelectionFrame(self, name="Reporter path:", path_var=self.reporter_path_var)
-        self.reporter_path.grid(row=2, column=0, sticky="we", padx=10, pady=5)
-
-        self.api_key = TextVarFrame(self, name="API key:", text_var=self.api_key_var)
-        self.api_key.grid(row=3, column=0, sticky="we", padx=10, pady=5)
-
-        self.ai_model_selector = DropDownSelectorFrame(self, name="AI model:", values=self.ai_models, selected_var=self.selected_ai_model)
-        self.ai_model_selector.grid(row=4, column=0, sticky="we", padx=10, pady=5)
-
-        self.prompt = LongTextVarFrame(self, name="AI prompt:", text_var=self.ai_prompt_var)
-        self.prompt.grid(row=5, column=0, sticky="we", padx=10, pady=5)
-
-        self.dual_model_switch = ctk.CTkSwitch(self, text="Dual AI Model analysis", command=self.on_dual_model_switch, variable=self.is_dual_model, onvalue="1", offvalue="0")
-        self.dual_model_switch.grid(row=6, column=0, sticky="we", padx=10, pady=5)
-
-        self.ai2_model_selector = DropDownSelectorFrame(self, name="AI model 2:", values=self.ai_models, selected_var=self.selected_ai_model2)
-
-        self.ai2_inlcude_profiler_output_checkbox = ctk.CTkCheckBox(self, text="Include profiler output", variable=self.ai2_include_profiler_output, offvalue="0", onvalue="1")
-
-        self.prompt2 = LongTextVarFrame(self, name="AI prompt 2:", text_var=self.ai_prompt_var2)
-
-        self.on_dual_model_switch()
+        self.settings_frame = SettingsFrame(self, self.reporter_path_var, self.api_key_var, self.ai_models, self.selected_ai_model_var, self.ai_prompt_var, self.is_dual_model_var, self.ai2_include_profiler_output_var, self.selected_ai_model2_var, self.ai_prompt2_var)
+        self.settings_frame.configure(corner_radius=0)
+        self.settings_frame.grid(row=0, column=0, sticky="nesw")
 
         self.bottom_buttons = BottomButtonsFrame(self, name1="Discard & Return", command1=self.on_cancel_button, name2="Save & Return", command2=self.on_save_button)
-        self.bottom_buttons.grid(row=99, column=0, pady=(5, 10), padx=10, sticky="s")
+        self.bottom_buttons.grid(row=99, column=0, pady=10, padx=10, sticky="s")
 
 
     def on_save_button(self):
         self.app_config["reporter_path"] = self.reporter_path_var.get()
         self.app_config["api_key"] = self.api_key_var.get()
-        self.app_config["selected_ai_model"] = self.selected_ai_model.get()
+        self.app_config["selected_ai_model"] = self.selected_ai_model_var.get()
         self.app_config["ai_prompt"] = self.ai_prompt_var.get()
 
-        self.app_config["dual_ai_model"] = self.is_dual_model.get()
-        self.app_config["ai2_include_profiler_output"] = self.ai2_include_profiler_output.get()
-        self.app_config["selected_ai_model2"] = self.selected_ai_model2.get()
-        self.app_config["ai_prompt2"] = self.ai_prompt_var2.get()
+        self.app_config["dual_ai_model"] = self.is_dual_model_var.get()
+        self.app_config["ai2_include_profiler_output"] = self.ai2_include_profiler_output_var.get()
+        self.app_config["selected_ai_model2"] = self.selected_ai_model2_var.get()
+        self.app_config["ai_prompt2"] = self.ai_prompt2_var.get()
 
         self.master.write_config(self.app_config)
         self.master.set_page("HomePage")
@@ -68,26 +48,84 @@ class SettingsPage(ctk.CTkFrame):
     def on_cancel_button(self):
         self.reporter_path_var.set(self.app_config["reporter_path"])
         self.api_key_var.set(self.app_config["api_key"])
-        self.selected_ai_model.set(self.app_config["selected_ai_model"])
+        self.selected_ai_model_var.set(self.app_config["selected_ai_model"])
         self.ai_prompt_var.set(self.app_config["ai_prompt"])
 
-        self.is_dual_model.set(self.app_config["dual_ai_model"])
-        self.ai2_include_profiler_output.set(self.app_config["ai2_include_profiler_output"])
-        self.selected_ai_model2.set(self.app_config["selected_ai_model2"])
-        self.ai_prompt_var2.set(self.app_config["ai_prompt2"])
+        self.is_dual_model_var.set(self.app_config["dual_ai_model"])
+        self.ai2_include_profiler_output_var.set(self.app_config["ai2_include_profiler_output"])
+        self.selected_ai_model2_var.set(self.app_config["selected_ai_model2"])
+        self.ai_prompt2_var.set(self.app_config["ai_prompt2"])
 
         self.master.set_page("HomePage")
 
-    def on_dual_model_switch(self):
-        if self.is_dual_model.get() == "1":
+
+class SettingsFrame(ctk.CTkScrollableFrame):
+    def __init__(self, master, reporter_path_var, api_key_var, ai_models, selected_ai_model_var, ai_prompt_var, is_dual_model_var, ai2_include_profiler_output_var, selected_ai_model2_var, ai_prompt2_var):
+        super().__init__(master)
+
+        self.reporter_path_var = reporter_path_var
+        self.api_key_var = api_key_var
+        self.ai_models = ai_models
+        self.selected_ai_model_var = selected_ai_model_var
+        self.ai_prompt_var = ai_prompt_var
+
+        self.is_dual_model_var = is_dual_model_var
+        self.ai2_include_profiler_output_var = ai2_include_profiler_output_var
+        self.selected_ai_model2_var = selected_ai_model2_var
+        self.ai_prompt2_var = ai_prompt2_var
+
+        self.toggle_advanced_settings_var = StringVar(self, value="0")
+
+        self.grid_columnconfigure(0, weight=1)
+
+        self.reporter_path = PathSelectionFrame(self, name="Reporter path:", path_var=self.reporter_path_var)
+        self.reporter_path.grid(row=1, column=0, sticky="we", padx=10, pady=(10,5))
+
+        self.api_key = TextVarFrame(self, name="Gemini API key:", text_var=self.api_key_var)
+        self.api_key.grid(row=2, column=0, sticky="we", padx=10, pady=5)
+
+        self.toggle_advanced_settings = ctk.CTkSwitch(self, text="Advanced settings",command=self.on_toggle_advanced_settings, variable=self.toggle_advanced_settings_var, offvalue="0", onvalue="1")
+        self.toggle_advanced_settings.grid(row=3, column=0, sticky="we", padx=11, pady=5)
+
+        self.ai_model_selector = DropDownSelectorFrame(self, name="AI model:", values=self.ai_models, selected_var=self.selected_ai_model_var)
+
+        self.prompt = LongTextVarFrame(self, name="AI prompt:", text_var=self.ai_prompt_var)
+
+        self.toggle_dual_model = ctk.CTkSwitch(self, text="Dual AI Model analysis", command=self.on_toggle_dual_model, variable=self.is_dual_model_var, onvalue="1", offvalue="0")
+
+        self.ai2_model_selector = DropDownSelectorFrame(self, name="AI model 2:", values=self.ai_models, selected_var=self.selected_ai_model2_var)
+
+        self.toggle_ai2_inlcude_profiler_output = ctk.CTkCheckBox(self, text="Include profiler output for AI 2", variable=self.ai2_include_profiler_output_var, offvalue="0", onvalue="1")
+
+        self.prompt2 = LongTextVarFrame(self, name="AI prompt 2:", text_var=self.ai_prompt2_var)
+
+
+    def on_toggle_advanced_settings(self):
+        if self.toggle_advanced_settings_var.get() == "1":
+            self.ai_model_selector.grid(row=4, column=0, sticky="we", padx=10, pady=5)
+            self.prompt.grid(row=5, column=0, sticky="we", padx=10, pady=5)
+            self.toggle_dual_model.grid(row=6, column=0, sticky="we", padx=11, pady=5)
+
+            self.on_toggle_dual_model()
+        else:
+            self.ai_model_selector.grid_forget()
+            self.prompt.grid_forget()
+            self.toggle_dual_model.grid_forget()
+
+            self.ai2_model_selector.grid_forget()
+            self.toggle_ai2_inlcude_profiler_output.grid_forget()
+            self.prompt2.grid_forget()
+
+
+    def on_toggle_dual_model(self):
+        if self.is_dual_model_var.get() == "1":
             self.ai2_model_selector.grid(row=7, column=0, sticky="we", padx=10, pady=5)
-            self.ai2_inlcude_profiler_output_checkbox.grid(row=8, column=0, sticky="we", padx=10, pady=5)
+            self.toggle_ai2_inlcude_profiler_output.grid(row=8, column=0, sticky="we", padx=11, pady=5)
             self.prompt2.grid(row=9, column=0, sticky="we", padx=10, pady=5)
         else:
             self.ai2_model_selector.grid_forget()
-            self.ai2_inlcude_profiler_output_checkbox.grid_forget()
+            self.toggle_ai2_inlcude_profiler_output.grid_forget()
             self.prompt2.grid_forget()
-
 
 
 class PathSelectionFrame(ctk.CTkFrame):
@@ -98,7 +136,7 @@ class PathSelectionFrame(ctk.CTkFrame):
         self.grid_columnconfigure((1, 2), weight=1)
 
         self.label = ctk.CTkLabel(self, text=name, width=100, anchor="w")
-        self.label.grid(row=0, column=0, sticky="w", padx=(10,5)) #sticky="swn", padx=(10, 5), pady=5)
+        self.label.grid(row=0, column=0, sticky="w", padx=(10,5))
 
         self.path_label = ctk.CTkLabel(self, textvariable=self.path_var, anchor="w", wraplength=400)
         self.path_label.grid(row=0, column=1, sticky="we")
@@ -148,7 +186,7 @@ class LongTextVarFrame(ctk.CTkFrame):
         self.label = ctk.CTkLabel(self, text=name, width=100, anchor="nw")
         self.label.grid(row=0, column=0, sticky="swn", padx=(10, 5), pady=5)
 
-        self.textbox = ctk.CTkTextbox(self, height=160)
+        self.textbox = ctk.CTkTextbox(self, height=150)
         self.textbox.grid(row=0, column=1, sticky="we")
 
         self.textbox.insert("0.0", self.text_var.get())
